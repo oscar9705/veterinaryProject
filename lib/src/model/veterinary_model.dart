8f7e9c7a984aae870eb2b2e70c7b59cc5fd60927
Oscar Salazar <salazaroscar9705@gmail.com>
2020-11-11 13:29:23 -0500

    add model veterinary

---
+++ lib/src/model/veterinary_model.dart
@@ -0,0 +1,38 @@
+class Veterinary {
+  int id;
+  String name;
+  String nit;
+  String email;
+  bool state;
+
+  Veterinary({
+    this.id,
+    this.name,
+    this.nit,
+    this.email,
+    this.state,
+  });
+
+  factory Veterinary.fromJsonMap(Map<String, dynamic> json) {
+    return new Veterinary(
+      id: json['idVet'],
+      name: json['nameVet'],
+      nit: json['nitVet'],
+      email: json['emailVet'],
+      state: json['stateVet'],
+    );
+  }
+  Map<String, dynamic> toJson() => {
+        'idVet': id,
+        'nameVet': name,
+        'nitVet': nit,
+        'emailVet': email,
+        'stateVet': state,
+      };
+  Map<String, dynamic> toJsonRegistry() => {
+        'nameVet': name,
+        'nitVet': nit,
+        'emailVet': email,
+        'stateVet': state,
+      };
+}
